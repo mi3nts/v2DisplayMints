5b4217fe416d302a342dc9052c7586ebac45cf49
lakithaomal <lhw150030@utdallas.edu>
2020-01-09 23:46:35 +0000

    DU

--- firmware/xu4/legacy/displayV2001.py
+++ firmware/xu4/legacy/displayV2001.py
@@ -17,16 +17,19 @@ SUP = str.maketrans("0123456789", "⁰¹²³⁴⁵⁶⁷⁸⁹")
 
 def showIntro():
 
-	os.system("vodisp bitmap mints1.bmp")
-	time.sleep(2)
-	os.system("vodisp bitmap mints2.bmp")
-	time.sleep(2)
-	os.system("vodisp bitmap mints5.bmp")
-	time.sleep(2)
-	os.system("vodisp bitmap mints4.bmp")
-	time.sleep(2)
-	os.system("vodisp bitmap mints3.bmp")
-	time.sleep(2)
+	os.system("vodisp bitmap mints50.bmp")
+	time.sleep(5)
+	os.system("vodisp bitmap mints02.bmp")
+	time.sleep(5)
+	os.system("vodisp bitmap mints05.bmp")
+	time.sleep(5)
+	os.system("vodisp bitmap mints04.bmp")
+	time.sleep(5)
+	os.system("vodisp bitmap mints03.bmp")
+	time.sleep(5)
+
+
+
 
 
 
@@ -41,12 +44,11 @@ while(True):
 		BME280,validBME280        = mL.readJSONLatestAll("BME280")
 		LIBRAD ,validLIBRAD       = mL.readJSONLatestAll("LIBRAD")
 		APDS9002,validAPDS9002    = mL.readJSONLatestAll("APDS9002")
-		TB108L,validTB108L        = mL.readJSONLatestAll("TB108L")
+
 
 		pm1   = str(OPCN3['pm1']).rjust(7," ")
 		pm2_5 = str(OPCN3['pm2_5']).rjust(7," ")
 		pm10  = str(OPCN3['pm10']).rjust(7," ")
-		ozone = str(TB108L['ozone']).rjust(7," ")
 
 		temperature = BME280['temperature']
 		pressure    = float(BME280['pressure'])/100
@@ -64,17 +66,16 @@ while(True):
 		d.text((1,55), "  PM1   = ".translate(SUB)  + pm1   + " mg/m3".translate(SUP) , font=fnt, fill="WHITE")
 		d.text((1,85), "  PM2.5 = ".translate(SUB)  + pm2_5 + " mg/m3".translate(SUP) , font=fnt, fill="WHITE")
 		d.text((1,115),"  PM10  = ".translate(SUB)  + pm10  + " mg/m3".translate(SUP) , font=fnt, fill="WHITE")
-		d.text((1,145),"  O3  = ".translate(SUB)  + ozone  + " ppb" , font=fnt, fill="WHITE")
-		d.text((1,175),"--------------------------------------------------------------------------------------------------", font=fnt, fill="WHITE")
-		d.text((1,205),"  Temperature ="   + temperature + " C", font=fnt, fill="WHITE")
-		d.text((1,235),"  Pressure    = "  + str(pressure) + " mbar"  , font=fnt, fill="WHITE")
-		d.text((1,265),"  Humidity    = "  + humidity+  " %", font=fnt, fill="WHITE")
-		d.text((1,295),"--------------------------------------------------------------------------------------------------", font=fnt, fill="WHITE")
-		d.text((1,325),"  Luminance = "   + luminance , font=fnt, fill="WHITE")
-		d.text((1,355),"  Radiation  = "  + radiation , font=fnt, fill="WHITE")
-		d.text((1,385),"--------------------------------------------------------------------------------------------------", font=fnt, fill="WHITE")
-		d.text((1,420),"  Last Updated ="   + lastUpdate , font=fnt, fill="WHITE")
-		d.text((1,455),"  MINTS (Multi-Scale Integrated Sensing and Simulation)", font=fnt, fill="GREEN")
+		d.text((1,145),"--------------------------------------------------------------------------------------------------", font=fnt, fill="WHITE")
+		d.text((1,175),"  Temperature ="   + temperature + " C", font=fnt, fill="WHITE")
+		d.text((1,205),"  Pressure    = "  + str(pressure) + " mbar"  , font=fnt, fill="WHITE")
+		d.text((1,235),"  Humidity    = "  + humidity+  " %", font=fnt, fill="WHITE")
+		d.text((1,265),"--------------------------------------------------------------------------------------------------", font=fnt, fill="WHITE")
+		d.text((1,295),"  Luminance = "   + luminance , font=fnt, fill="WHITE")
+		d.text((1,325),"  Radiation  = "  + radiation , font=fnt, fill="WHITE")
+		d.text((1,355),"--------------------------------------------------------------------------------------------------", font=fnt, fill="WHITE")
+		d.text((1,385),"  Last Updated ="   + lastUpdate , font=fnt, fill="WHITE")
+		d.text((1,450),"  MINTS (Multi-Scale Integrated Sensing and Simulation)", font=fnt, fill="GREEN")
 		rotated_image = imgBase.rotate(90)
 		flipped_img   = rotated_image.transpose(Image.FLIP_LEFT_RIGHT)
 		flipped_img.save("mintsLive.bmp")
